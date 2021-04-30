ceb9c62ae2a133fc225dd6f60ba7dac73cd4a5bc
chenhaoliang <chenhaoliang@tuzhanai.com>
2021-04-30 19:07:47 +0800

    更新

--- src/components/phaserStudyAll/gameFour/game.js
+++ src/components/phaserStudyAll/gameFour/game.js
@@ -5,9 +5,23 @@ const Phaser = window.Phaser;
 const Delay = 600;
 const Count = 6;
 
+// 绳子定位
+const ropeY = 190;
+
 // 炮筒定位
 const gunBodyX = 375;
 const gunBodyY = 810 + 151 / 2;
+
+// 车轮定位
+const wheelLeftX = 750 * 0.48;
+const wheelLeftY = 860;
+const wheelRightX = 750 * 0.575;
+const wheelRightY = 850;
+
+// 球的定位
+const ballX = 375;
+const ballY = 810;
+const speed = 500; // 球的飞行速度(垂直)-每次运动的y轴距离
 
 class MainScene extends Phaser.Scene {
   constructor() {
@@ -17,8 +31,8 @@ class MainScene extends Phaser.Scene {
 
     this.gunBodyTween = null; // 炮筒动画
 
-    this.gunAngle = null; // 炮筒旋转角度
-    this.maxAngle = (Math.atan(gunBodyX / gunBodyY) * 180) / Math.PI; // 最大旋转角度
+    this.gunAngle = 0; // 炮筒旋转角度
+    this.maxAngle = (Math.atan(gunBodyX / (gunBodyY - ropeY)) * 180) / Math.PI; // 最大旋转角度
   }
   preload() {
     this.load.image("gunBody", require("@/assets/images/gun/gun_body.png"));
@@ -40,37 +54,43 @@ class MainScene extends Phaser.Scene {
 
     this.rope = this.add
       .image(750, 79, "rope", 0)
-      .setPosition(375, 190)
+      .setPosition(375, ropeY)
       .setVisible(true);
 
-    this.ball = this.add
+    // this.ball = this.add
+    //   .image(64, 64, "ball", 0)
+    //   .setPosition(ballX, ballY)
+    //   .setVisible(true)
+    //   .setDepth(1);
+
+    this.ball = this.physics.add
       .image(64, 64, "ball", 0)
       .setPosition(375, 800)
-      .setVisible(true);
-
-    // this.ball = this.physics.add
-    //   .image(64, 64, "ball", 0)
-    //   .setPosition(375, 800)
-    //   .setVisible(true)
-    //   .setCollideWorldBounds(true)
-    //   .setBounce(1);
+      .setVisible(false)
+      .setCollideWorldBounds(false)
+      .setBounce(1)
+      .setCircle(32)
+      .setBounce(0);
 
     this.createAward();
     this.createLink();
+
+    this.physics.add.collider(this.ball, this.packGroup, this.colliderCb);
 
     this.gunInit();
 
     this.addEvent();
   }
 
+  // 初始化炮台
   gunInit() {
     this.wheelLeft = this.add
       .image(142, 93, "wheelLeft", 0)
-      .setPosition(750 * 0.48, 860)
+      .setPosition(wheelLeftX, wheelLeftY)
       .setVisible(true);
     this.wheelRight = this.add
       .image(56, 73, "wheelRight", 0)
-      .setPosition(750 * 0.575, 850)
+      .setPosition(wheelRightX, wheelRightY)
       .setVisible(true);
     this.gunBody = this.add
       .image(103, 151, "gunBody", 0)
@@ -86,26 +106,35 @@ class MainScene extends Phaser.Scene {
       ease: "Sine.easeInOut",
       repeat: 0,
       yoyo: true,
-      complete: () => {
-        console.log(this.ball, "ball");
+      onComplete: () => {
+        this.ball.setVisible(true); // 显示球
+
+        // 根据角度 计算出xy的比例
+        let persent = Math.tan(this.gunAngle * ((2 * Math.PI) / 360));
+
+        // 根据y轴速度(每次移动距离) 计算出x轴速度
+        let speedX = speed * persent;
+        this.ball.setVelocity(speedX, -speed);
       }
     });
   }
 
+  // 创建福袋
   createAward() {
     var awardStartPoint = new Phaser.Math.Vector2(850, 216);
     var controlPoint1 = new Phaser.Math.Vector2(375, 370);
     var endPoint = new Phaser.Math.Vector2(-100, 216);
     this.awardCurve = new Phaser.Curves.QuadraticBezier(awardStartPoint, controlPoint1, endPoint);
 
-    this.packGroup = this.add.group({
+    this.packGroup = this.physics.add.group({
       defaultKey: "award",
       maxSize: 6,
       createCallback: award => {
         award
           .setSize(200, 190)
           .setName("award" + this.packGroup.getLength())
-          .setScale(0.5);
+          .setScale(0.5)
+          .setCircle(100);
       }
     });
 
@@ -130,6 +159,7 @@ class MainScene extends Phaser.Scene {
     }
   }
 
+  // 创建福袋连接器
   createLink() {
     var awardStartPoint = new Phaser.Math.Vector2(850, 160);
     var controlPoint1 = new Phaser.Math.Vector2(375, 310);
@@ -161,28 +191,46 @@ class MainScene extends Phaser.Scene {
     }
   }
 
+  // 添加事件
   addEvent() {
     // 滑动屏幕操作
     this.input.on("pointermove", this.handleTouch);
   }
 
+  // 滑动屏幕回调
   handleTouch = e => {
     let activeX = e.x;
     let activeY = e.y;
 
     let gunDeg = (Math.atan((activeX - gunBodyX) / (gunBodyY - activeY)) * 180) / Math.PI;
-    let deg = gunDeg > 0 ? Math.min(gunDeg, this.maxAngle) : Math.max(gunDeg, -this.maxAngle);
+    let deg = (this.gunAngle = gunDeg > 0 ? Math.min(gunDeg, this.maxAngle) : Math.max(gunDeg, -this.maxAngle));
+
+    let gunH = this.gunBody.height;
 
     if (activeY < gunBodyY) {
       this.gunBody.setAngle(deg);
-      // this.wheelLeft.setX(this.wheelLeft.getBottomLeft().x + (deg * 750) / 800); // x轴一直在改变
+      this.wheelLeft.setX(wheelLeftX + deg);
+      this.wheelRight.setX(wheelRightX + deg);
+
+      // 根据角度 计算球对于初始点 移动的距离 设置球的位置
+      let w = parseInt(Math.sin(deg * ((2 * Math.PI) / 360)) * (gunH / 2));
+      let h = Math.abs(parseInt(Math.cos(deg * ((2 * Math.PI) / 360)) * (gunH / 2)));
+      let x = ballX + w;
+      let y = ballY + (gunH / 2 - h);
+      this.ball.setX(x);
+      this.ball.setY(y);
     }
+  };
+
+  // 碰撞回调
+  colliderCb = (obj1, obj2) => {
+    this.ball.setVisible(false).setPosition(ballX, ballY); // 显示球
+
+    console.log(obj2);
   };
 
   // 发球
   launchBall() {
-    console.log(this.gunBody, "this.gunBody");
-    console.log(this.gunBodyTween, "this.gunBodyTween");
     this.gunBodyTween.play();
   }
 
@@ -224,8 +272,8 @@ const game = {
       physics: {
         default: "arcade",
         arcade: {
-          debug: true,
-          gravity: { y: 200 }
+          debug: false,
+          gravity: { y: 0 }
         }
       }
     });
